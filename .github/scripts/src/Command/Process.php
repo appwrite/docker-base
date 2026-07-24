d3ee4e8c9a7d20d78361efb1b2554dc86f20c507
Jake Barnby <jakeb994@gmail.com>
2026-07-24 19:01:59 +1200

    refactor: use utopia cli and console libraries

--- .github/scripts/src/Command/Process.php
+++ .github/scripts/src/Command/Process.php
@@ -5,93 +5,49 @@ declare(strict_types=1);
 namespace DockerBase\Command;
 
 use Override;
+use Utopia\Console;
 
 final readonly class Process implements Runner
 {
-    /**
-     * @param array<string, string>|null $environment
-     */
-    public function __construct(
-        private ?string $directory = null,
-        private ?array $environment = null,
-    ) {
+    /** @param array<string, string>|null $environment */
+    public function __construct(private ?string $directory = null, private ?array $environment = null)
+    {
     }
 
-    /**
-     * @param list<string> $command
-     */
+    /** @param list<string> $command */
     #[Override]
     public function run(array $command, bool $check = true): Result
     {
         if ($command === [] || in_array('', $command, true)) {
-            throw new Exception(
-                $command,
-                message: 'A command must contain non-empty arguments',
-            );
+            throw new Exception($command, message: 'A command must contain non-empty arguments');
         }
 
-        $input = tmpfile();
-        $output = tmpfile();
-        $error = tmpfile();
-
-        if ($input === false || $output === false || $error === false) {
-            foreach ([$input, $output, $error] as $stream) {
-                if (is_resource($stream)) {
-                    fclose($stream);
-                }
-            }
-
-            throw new Exception(
-                $command,
-                message: 'Unable to allocate command output streams',
-            );
+        $stdout = '';
+        $stderr = '';
+        $previous = getcwd();
+        $environment = [];
+        foreach ($this->environment ?? [] as $key => $value) {
+            $environment[$key] = getenv($key);
+            putenv($key . '=' . $value);
         }
 
         try {
-            $pipes = [];
-            $process = proc_open(
-                $command,
-                [
-                    0 => $input,
-                    1 => $output,
-                    2 => $error,
-                ],
-                $pipes,
-                $this->directory,
-                $this->environment,
-                ['bypass_shell' => true],
-            );
-
-            if (! is_resource($process)) {
-                throw new Exception(
-                    $command,
-                    message: 'Unable to start command',
-                );
+            if ($this->directory !== null && ! chdir($this->directory)) {
+                throw new Exception($command, message: 'Unable to start command');
             }
-
-            $code = proc_close($process);
-            rewind($output);
-            rewind($error);
-            $standardOutput = stream_get_contents($output);
-            $standardError = stream_get_contents($error);
-
-            if ($standardOutput === false || $standardError === false) {
-                throw new Exception(
-                    $command,
-                    message: 'Unable to read command output',
-                );
-            }
-
-            $result = new Result($code, $standardOutput, $standardError);
+            $code = Console::execute($command, '', $stdout, $stderr);
+            $result = new Result($code, $stdout, $stderr);
             if ($check && ! $result->succeeded()) {
                 throw new Exception($command, $result);
             }
-
             return $result;
         } finally {
-            fclose($input);
-            fclose($output);
-            fclose($error);
+            if ($previous !== false) {
+                chdir($previous);
+            }
+            foreach ($environment as $key => $value) {
+                putenv($key . ($value === false ? '' : '=' . $value));
+            }
         }
     }
 }
